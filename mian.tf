9ec6fc05dd8668f8418b0220a191163e7c1eef9a
Bhishman <bh352449@dal.ca>
2024-06-26 15:32:55 -0300

    feat: crating a map and a list variable and accessing their values in tf script

--- mian.tf
+++ mian.tf
@@ -2,3 +2,8 @@ resource "local_file" "pets" {
   filename = "./pet.txt"
   content  = var.content
 }
+
+resource "local_file" "testing_variables" {
+  filename = "./testing_variables.txt"
+  content = "Accessing value at index 2: ${var.list[2]} and this is the map age: ${var.map["age"]}"
+}
--- variables.tf
+++ variables.tf
@@ -1,3 +1,27 @@
 variable "content" {
   default = "I've a pet which is a horse! (this content is coming from a variable!"
 }
+
+/* Variable of string type */
+variable "string_value" {
+  type    = string
+  default = "This is a string!"
+}
+
+/* Variable of map type */
+variable "map" {
+  type = object({
+    name = string
+    age  = number
+  })
+  default = {
+    "name" = "Bhishman"
+    "age"  = 22
+  }
+}
+
+/* Variable of list type */
+variable "list" {
+  type = list(any)
+  default = ["index0", 1, "index2"]
+}
