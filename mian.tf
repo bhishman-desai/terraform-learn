aaae193a17fe2201fd0d486f05863cc5d6cb3063
Bhishman <bh352449@dal.ca>
2024-06-26 15:55:11 -0300

    feat: linking two resources and passing data from one resource to another

--- mian.tf
+++ mian.tf
@@ -2,3 +2,10 @@ resource "local_file" "all_variables" {
   filename = "./all_variables.txt"
   content  = "String: ${var.string_value}, Number: ${var.number_value}, Bool: ${var.bool_value}, List: ${join(", ", var.list_value)}, Set: ${join(", ", var.set_value)}, Map: ${jsonencode(var.map_value)}, Object: ${jsonencode(var.object_value)}, Tuple: ${join(", ", var.tuple_value)}"
 }
+
+resource "random_uuid" "userId" {}
+
+resource "local_file" "Users" {
+  filename = "./user.txt"
+  content = "The user id is: ${random_uuid.userId.id}"
+}
