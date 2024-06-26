5cccca568551d524317b6b377e93c9d7d762ee08
Bhishman <bh352449@dal.ca>
2024-06-26 15:13:27 -0300

    feat: creating a "content" variable and passing it into resource

--- mian.tf
+++ mian.tf
@@ -1,4 +1,4 @@
 resource "local_file" "pets" {
   filename = "./pet.txt"
-  content  = "I've a pet which is a horse!"
+  content  = var.content
 }
---
+++ variables.tf
@@ -0,0 +1,3 @@
+variable "content" {
+  default = "I've a pet which is a horse! (this content is coming from a variable!"
+}
